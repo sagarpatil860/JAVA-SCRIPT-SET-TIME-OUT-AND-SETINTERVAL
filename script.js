f3929a92a289cb184dcf67ec08c241a04c7f8544
DESKTOP-H0R2DF3\Sagarpatil <sagarpatil860@gmail.com>
2021-11-02 02:18:48 +0530

    no message

--- script.js
+++ script.js
@@ -1,23 +1,40 @@
 // let timerId=setTimeout(a,x);
 
 let y=document.getElementById("x");
-let numb=0;
-// y.addEventListener("input" ,function v(){ numb=y.value;
-//     console.log(numb);
-//     return(numb);
+let inputnum=0;
+let t;
+// let startTime;
+// y.addEventListener("input" ,function v(){ inputnum=y.value;
+//     console.log(inputnum);
+//     return(inputnum);
     
 // });
 
-document.getElementById("d").addEventListener("click",()=>{numb=y.value;console.log(numb);});
+document.getElementById("d").addEventListener("click" ,()=>{
+    inputnum=parseInt(y.value);
+    
+});
 
-document.getElementById("d").addEventListener("click", setTimeout.bind(null,a,numb));
+// setTimeout(()=>console.log(inputnum),inputnum);
 
-function a (){
-    console.log(numb);
-    console.log(y.value);
+document.getElementById("d").addEventListener("click", l);
+function l()
+{
+  
+setInterval (function a (){
+    
+
+    console.log(inputnum);
+    // console.log(y.value);
     console.log("in function");
     let b=document.createElement("p");
-b.innerHTML="sagar";
+    b.innerHTML="sagar";
 
-document.body.appendChild(b);
+    document.body.appendChild(b);
+    
+},parseInt(y.value) );}
+document.getElementById("g").addEventListener("click", o)
+function o()
+{
+    clearInterval(t)
 }
